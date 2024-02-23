db0257c72fffe9540ed0963b3cfbc9cc822bb36a
klabarre <klabarre@dawan.fr>
2024-02-23 11:40:47 +0100

    MàJ pour auth

--- src/view/Authentification.jsx
+++ src/view/Authentification.jsx
@@ -1,6 +1,53 @@
+import { useEffect, useState } from "react";
+
 /* eslint-disable react/no-unescaped-entities */
 export const Authentification = () => {
-  return (
+  
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [isLogginIn, setIsLogginIn] = useState(false)
+    const [loginError, setLogginError] = useState('')
+
+    const handleLogin = async () => {
+        setIsLogginIn(true);
+        try{
+            // Ici, ajoutez le véritable appel d'API pour s'authentifier
+            // (en attendant, on va juste simuler avec un setTimeout)
+
+            await new Promise(resolve => setTimeout(resolve, 1000))
+
+            // A la place de 'notreToken', sauvegarder le Token reçu en réponse a notre requete POST
+            const authToken = 'notreToken'
+
+            // localStorage -> clé/value
+            localStorage.setItem('authToken', authToken)
+            
+            // Si je veux récupérer mon token je dois faire 'localStorage.getItem(Ma_clé)':
+            console.log(localStorage.getItem('authToken'))
+
+            // Je simule une erreur
+            throw new Error ("Erreur d'authentification")
+        }
+        catch (error){
+            setLogginError("Nom d'utilisateur ou mot de passe incorrect")
+        } 
+        finally {
+            setIsLogginIn(false)
+        }
+    }
+
+    useEffect( () => {
+        // Logique à exécuter après la tentative de connexion
+        if(!isLogginIn && !loginError){
+            console.log('Authentification réussi')
+
+            // Ce que fait quand on a réussi à se connecter
+
+        }
+    }, [isLogginIn, loginError])
+
+    return (
     <>
       <div className="flex items-center justify-center min-h-screen">
         <div className="p-8 bg-white rounded shadow-md w-96">
@@ -12,8 +59,8 @@ export const Authentification = () => {
             type="text"
             id='username'
             className="w-full px-3 py-2 mt-1 text-black border border-gray-300 rounded"
-            value={}
-            onChange={}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -22,17 +69,15 @@ export const Authentification = () => {
                 type="password" 
                 id="password"
                 className="w-full px-3 py-2 mt-1 text-black border border-gray-300 rounded"
-                value={}
-                onChange={}
+                value={password}
+                onChange={ (e) => setPassword(e.target.value) }
             />
           </div>
-
           <button 
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            onClick={}
-            disabled={false}
-            >
-                
+            onClick={handleLogin}
+            disabled={isLogginIn}
+            >  
           </button>
         </div>
       </div>
